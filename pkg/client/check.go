a64139e54dd8d445398531eba8fe269d8e0da5db
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-09-22 10:31:14 +0530

    Remove event handler
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- pkg/client/check.go
+++ pkg/client/check.go
@@ -1,60 +1,36 @@
 package client
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/cache"
 )
 
-const (
-	NAMESPACE = "conformance"
-	POD_NAME  = "e2e-conformance-test"
-)
-
 // Check for Pod and start a go routine if new deployment added
 func (c *Client) CheckForE2ELogs() {
-	labelOptions := informers.WithTweakListOptions(func(opts *metav1.ListOptions) {
-	})
-
-	informerFactory := informers.NewSharedInformerFactoryWithOptions(c.ClientSet, 2*time.Minute,
-		informers.WithNamespace(NAMESPACE), labelOptions)
+	informerFactory := informers.NewSharedInformerFactory(c.ClientSet, 10*time.Second)
 
 	podInformer := informerFactory.Core().V1().Pods()
-	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			log.Println("Pod added. Let's start checking!")
 
-			ch := make(chan error)
-			done := make(chan bool)
-
-			go c.getLogs(ch, done)
-
-		loop:
-			for {
-				select {
-				case err := <-ch:
-					log.Fatalf("error getting logs: %v", err)
-				case <-done:
-					break loop
-				}
-			}
-		},
-	})
+	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{})
 
 	informerFactory.Start(wait.NeverStop)
 	informerFactory.WaitForCacheSync(wait.NeverStop)
+
+	c.getLogs()
 }
 
-func (c *Client) getLogs(ch chan error, done chan bool) {
-	err := GetPodLogs(c.ClientSet)
-	if err != nil {
-		ch <- fmt.Errorf("get logs: %s", err.Error())
-	}
+func (c *Client) getLogs() {
+	cancelCtx := context.Background()
+	cancelCtx, cancelFunc := context.WithCancel(cancelCtx)
+	defer cancelFunc()
 
-	done <- true
+	err := getPodLogs(cancelCtx, c.ClientSet)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
