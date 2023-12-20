a38617245f09cd123b69d7a1e88aa94e4f33dcbb
Davanum Srinivas <davanum@gmail.com>
2023-12-20 15:59:12 -0500

    Add support for --skip (to skip tests)
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- pkg/service/args.go
+++ pkg/service/args.go
@@ -27,6 +27,9 @@ type ArgConfig struct {
 	// e.g. - sig-auth, sig-apps
 	Focus string
 
+	// Skip set the E2E_SKIP env var to skip specified tests
+	Skip string
+
 	// Image let's people use the conformance container image of their own choice
 	// Get the list of images from https://console.cloud.google.com/gcr/images/k8s-artifacts-prod/us/conformance
 	// default registry.k8s.io/conformance:v1.28.0
@@ -36,7 +39,8 @@ type ArgConfig struct {
 func InitArgs() (*ArgConfig, error) {
 	var cfg ArgConfig
 
-	flag.StringVar(&cfg.Focus, "focus", "", "focus runs a specific e2e test. e.g. - sig-auth")
+	flag.StringVar(&cfg.Focus, "focus", "", "focus runs a specific e2e test. e.g. - sig-auth. allows regular expressions.")
+	flag.StringVar(&cfg.Skip, "skip", "", "skip specific tests. allows regular expressions.")
 	flag.StringVar(&cfg.Image, "image", containerImage,
 		"image let's you select your conformance container image of your choice. for example, for v1.28.0 version of tests, use - 'registry.k8s.io/conformance-amd64:v1.25.0'")
 
