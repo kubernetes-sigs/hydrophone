db401a79a957be8322d965480bfd68db0e97f63c
Davanum Srinivas <davanum@gmail.com>
2023-12-23 19:56:34 -0600

    Script to update k8s version
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- go.mod
+++ go.mod
@@ -44,9 +44,9 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
-	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	k8s.io/kube-openapi v0.0.0-20231214164306-ab13479f8bf8 // indirect
+	k8s.io/utils v0.0.0-20231127182322-b307cd553661 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
---
+++ hack/bump-k8s.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+HYDROPHONE_ROOT=${SCRIPTDIR}/../
+
+pushd "${HYDROPHONE_ROOT}" >/dev/null
+  go mod edit -json | jq -r ".Require[] | .Path | select(contains(\"k8s.io/\"))" | xargs xargs -L1 go get -d
+  go mod tidy
+
+  K8S_VERSION=$(curl https://cdn.dl.k8s.io/release/stable.txt -s)
+  sed -i "s|K8S_VERSION: .*|K8S_VERSION: $K8S_VERSION|" .github/workflows/*.yml
+  sed -i "s|conformance:v(\d+\.)?(\d+\.)?(\d+)||" pkg/common/*.go
+  sed -i "s|conformance:v(\d+\.)?(\d+\.)?(\d+)||" README.md
+
+popd >/dev/null
+git status
