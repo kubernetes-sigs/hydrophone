fa945c3eb8db2dc1a5d10d2d0a3c8945620ad04b
Balaaditya Patanjali <138308724+BalaadityaPatanjali@users.noreply.github.com>
2024-01-20 04:56:32 +0530

    Update list_images.go

--- pkg/service/list_images.go
+++ pkg/service/list_images.go
@@ -31,7 +31,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 // PrintListImages creates and runs a conformance image with the --list-images flag
