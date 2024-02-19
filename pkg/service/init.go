63b16e280f54a8035634065ad9413c0f5b999801
shivam <shivambharadwaj822@gmail.com>
2024-02-19 23:18:09 +0530

    Improved logging for clusterrole and clusterbinding
    Signed-off-by: shivam <shivambharadwaj822@gmail.com>

--- pkg/service/init.go
+++ pkg/service/init.go
@@ -247,8 +247,9 @@ func RunE2E(clientset *kubernetes.Clientset) {
 		} else {
 			log.Fatal(err)
 		}
+	} else {
+		log.Printf("clusterrole created %s\n", clusterRole.Name)
 	}
-	log.Printf("clusterrole created %s\n", clusterRole.Name)
 
 	clusterRoleBinding, err := clientset.RbacV1().ClusterRoleBindings().Create(ctx, &conformanceClusterRoleBinding, metav1.CreateOptions{})
 	if err != nil {
@@ -257,8 +258,9 @@ func RunE2E(clientset *kubernetes.Clientset) {
 		} else {
 			log.Fatal(err)
 		}
+	} else {
+		log.Printf("clusterrolebinding created %s\n", clusterRoleBinding.Name)
 	}
-	log.Printf("clusterrolebinding created %s\n", clusterRoleBinding.Name)
 
 	if viper.GetString("test-repo-list") != "" {
 		RepoListData, err := os.ReadFile(viper.GetString("test-repo-list"))
