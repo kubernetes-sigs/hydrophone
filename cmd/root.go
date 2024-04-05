c35f443563cbc4cdefc6905affeb0e00fde55c84
Marvin Beckers <marvin@kubermatic.com>
2024-04-05 08:20:22 +0200

    ignore unchecked errors temporarily to reduce PR surface
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- cmd/root.go
+++ cmd/root.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//nolint:errcheck // TODO(embik): temporary put in place to ensure PRs do not conflict
 package cmd
 
 import (
