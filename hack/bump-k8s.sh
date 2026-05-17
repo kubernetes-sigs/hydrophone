55b2429edb67ead0b87f768ce61cfcfb7ee61e40
Davanum Srinivas <davanum@gmail.com>
2026-05-17 07:56:30 +0300

    ci: fix update-deps workflow
    - Use dl.k8s.io instead of cdn.dl.k8s.io for the stable.txt lookup;
      the CDN host was failing with curl exit code 6 in the runner.
    - Bump actions/checkout to v6.0.2 and actions/setup-go to v6.4.0 so
      the workflows run on Node.js 24 ahead of the June 2026 deprecation.
    
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- hack/bump-k8s.sh
+++ hack/bump-k8s.sh
@@ -23,7 +23,7 @@ pushd "${HYDROPHONE_ROOT}" >/dev/null
   go mod edit -json | jq -r ".Require[] | .Path | select(contains(\"k8s.io/\"))" | xargs -L1 go get -d
   go mod tidy
 
-  K8S_VERSION=$(curl https://cdn.dl.k8s.io/release/stable.txt -s)
+  K8S_VERSION=$(curl https://dl.k8s.io/release/stable.txt -s)
   sed -i "s|K8S_VERSION: .*|K8S_VERSION: $K8S_VERSION|" .github/workflows/*.yml
   sed -i -r "s/conformance:v[0-9]+\.[0-9]+\.[0-9]+/conformance:$K8S_VERSION/g" README.md pkg/common/*.go
 
